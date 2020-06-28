57dc95b6051250bd9edc6b8645cbbcedf20b0416
Clemens Rabe <clemens.rabe@clemensrabe.de>
2020-06-28 13:47:15 +0200

    Prepared functional tests.

---
+++ tests/functional_tests/helpers/output_helpers.sh
@@ -0,0 +1,79 @@
+# ----------------------------------------------------------------------------
+# Helper functions for the functional tests.
+#
+# Copyright (c) 2020 by Clemens Rabe <clemens.rabe@clemensrabe.de>
+# All rights reserved.
+# This file is part of gitcache (https://github.com/seeraven/gitcache)
+# and is released under the "BSD 3-Clause License". Please see the LICENSE file
+# that is included as part of this package.
+# ----------------------------------------------------------------------------
+
+# Usage: capture_output <expected retval> <name> <args>
+function capture_output()
+{
+    STDOUT_FILE=$(tempfile)
+    STDERR_FILE=$(tempfile)
+
+    EXPECTED_RETVAL=$1
+    shift
+
+    EXPECTED_STDOUT_FILE=${EXPECTED_OUTPUT_DIR}/${EXPECTED_OUTPUT_PREFIX}_$1_stdout.txt
+    EXPECTED_STDERR_FILE=${EXPECTED_OUTPUT_DIR}/${EXPECTED_OUTPUT_PREFIX}_$1_stderr.txt
+    shift
+
+    set +e
+    $GITCACHE_BIN $@ > $STDOUT_FILE 2> $STDERR_FILE
+    RETVAL=$?
+    set -e
+
+    if [ $RETVAL != $EXPECTED_RETVAL ]; then
+	echo "ERROR: Command gitcache $@ gave unexpected return value $RETVAL (expected ${EXPECTED_RETVAL})"
+	RETVAL=10
+    else
+	RETVAL=0
+    fi
+
+    if [ "$SAVE_REFERENCE" == "1" ]; then
+	cp $STDOUT_FILE $EXPECTED_STDOUT_FILE
+	cp $STDERR_FILE $EXPECTED_STDERR_FILE
+    else
+	if ! cmp -s $STDOUT_FILE $EXPECTED_STDOUT_FILE; then
+	    echo "ERROR: Command gitcache $@ gave unexpected stdout output:"
+	    cat $STDOUT_FILE
+	    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+	    echo "Diff:"
+	    diff $STDOUT_FILE $EXPECTED_STDOUT_FILE
+	    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+	    RETVAL=10
+	fi
+	if ! cmp -s $STDERR_FILE $EXPECTED_STDERR_FILE; then
+	    echo "ERROR: Command gitcache $@ gave unexpected sterr output:"
+	    cat $STDERR_FILE
+	    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+	    echo "Diff:"
+	    diff $STDERR_FILE $EXPECTED_STDERR_FILE
+	    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+	    RETVAL=10
+	fi
+    fi
+
+    rm -f $STDOUT_FILE $STDERR_FILE
+
+    return $RETVAL
+}
+
+# Usage: capture_output_success <name> <args>
+function capture_output_success()
+{
+    capture_output 0 $@
+}
+
+# Usage: capture_output_failure <name> <args>
+function capture_output_failure()
+{
+    capture_output 1 $@
+}
+
+# -----------------------------------------------------------------------------
+# EOF
+# -----------------------------------------------------------------------------
---
+++ tests/functional_tests/run_tests.sh
@@ -0,0 +1,110 @@
+#!/bin/bash -eu
+# ----------------------------------------------------------------------------
+# Functional tests for the gitcache command
+#
+# Copyright (c) 2020 by Clemens Rabe <clemens.rabe@clemensrabe.de>
+# All rights reserved.
+# This file is part of gitcache (https://github.com/seeraven/gitcache)
+# and is released under the "BSD 3-Clause License". Please see the LICENSE file
+# that is included as part of this package.
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+#  SETTINGS
+# ----------------------------------------------------------------------------
+export TEST_BASE_DIR=$(dirname $(readlink -f $0))
+BASE_DIR=$(dirname $(dirname $TEST_BASE_DIR))
+GITCACHE_BIN=${BASE_DIR}/gitcache
+
+
+# -----------------------------------------------------------------------------
+# CHECK COMMAND LINE ARGUMENTS
+# -----------------------------------------------------------------------------
+SAVE_REFERENCE=0
+VERBOSE=0
+WAIT_ON_ERROR=0
+
+function usage() {
+    echo
+    echo "Usage: $1 [-h] [-s] [-c] [-v]"
+    echo ""
+    echo "Functional tests of gitcache."
+    echo
+    echo "Options:"
+    echo " -h                : Print this help."
+    echo " -s                : Save the gitcache output as reference."
+    echo " -c                : Use the coverage wrapper."
+    echo " -v                : Be more verbose."
+    exit 1
+}
+
+while getopts ":hscv" OPT; do
+    case $OPT in
+	h )
+	    usage $0
+	    ;;
+	s )
+	    SAVE_REFERENCE=1
+	    ;;
+	c )
+	    GITCACHE_BIN="coverage run --append --rcfile=${BASE_DIR}/.coveragerc-functional $GITCACHE_BIN"
+	    ;;
+	v )
+	    VERBOSE=1
+	    ;;
+	\? )
+	    usage $0
+	    ;;
+    esac
+done
+
+
+# -----------------------------------------------------------------------------
+# EXPORTS FOR TEST SCRIPTS
+# -----------------------------------------------------------------------------
+export EXPECTED_OUTPUT_DIR=$TEST_BASE_DIR/expected
+mkdir -p ${EXPECTED_OUTPUT_DIR}
+export GITCACHE_BIN
+export SAVE_REFERENCE
+
+export TMP_WORKDIR=$(mktemp --directory)
+export GITCACHE_DIR=$(mktemp --directory)
+export GITCACHE_REAL_GIT=/usr/bin/git
+
+
+# -----------------------------------------------------------------------------
+# RUN TESTS
+# -----------------------------------------------------------------------------
+RETVAL=0
+TMPOUTPUT=$(tempfile)
+
+for SCRIPT in ${TEST_BASE_DIR}/tests/*.sh; do
+    echo -n "Running test $(basename $SCRIPT) ... "
+    if $SCRIPT &> $TMPOUTPUT; then
+	echo "OK"
+	if [[ $VERBOSE -eq 1 ]]; then
+	    echo "-----------------------------------------------------------------"
+	    echo " Output:"
+	    cat $TMPOUTPUT
+	    echo "-----------------------------------------------------------------"
+	fi
+    else
+	echo "FAILED"
+	echo "-----------------------------------------------------------------"
+	echo " Output:"
+	cat $TMPOUTPUT
+	echo "-----------------------------------------------------------------"
+	RETVAL=1
+    fi
+done
+
+rm -f $TMPOUTPUT
+rm -rf $GITCACHE_DIR $TMP_WORKDIR
+
+exit $RETVAL
+
+
+# -----------------------------------------------------------------------------
+# EOF
+# -----------------------------------------------------------------------------
